b4a40a53097b5e39c0b271c584ad8b24bdebb75d
Ivan Novakov <ivan.novakov@debug.cz>
2012-11-12 21:01:50 +0100

    added simple example

---
+++ experiments/ecp_client.php
@@ -0,0 +1,21 @@
+<?php
+
+use Saml\Ecp\Client\Client;
+use Saml\Ecp\Discovery\Method\StaticIdp;
+use Saml\Ecp\Authentication\Method\BasicAuth;
+
+require __DIR__ . '/../vendor/autoload.php';
+require __DIR__ . '/_common.php';
+
+$credentials = $globalConfig->get('credentials');
+$authenticationMethod = new BasicAuth($credentials->toArray());
+
+$discoveryOptions = $globalConfig->get('discovery')
+    ->get('options');
+$discoveryMethod = new StaticIdp($discoveryOptions->toArray());
+
+$client = new Client($globalConfig->get('client'));
+$response = $client->authenticate($authenticationMethod, $discoveryMethod);
+
+_dump((string) $response->getHttpResponse());
+
