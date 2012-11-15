369f48c8c2ea78f5b302ceec7573a28548c18feb
Ivan Novakov <ivan.novakov@debug.cz>
2012-11-15 16:26:30 +0100

    removed the validate() method

--- lib/Saml/Ecp/Response/SpInitialResponse.php
+++ lib/Saml/Ecp/Response/SpInitialResponse.php
@@ -7,25 +7,4 @@ use Saml\Ecp\Client\MimeType;
 
 class SpInitialResponse extends AbstractResponse
 {
-
-
-    public function validate ()
-    {
-        $this->_validateStatusCode();
-        
-        $httpResponse = $this->getHttpResponse();
-        
-        $contentType = $httpResponse->getHeaders()
-            ->get('Content-Type');
-        
-        if (! $contentType || $contentType->getFieldValue() != MimeType::PAOS) {
-            throw new Exception\InvalidContentTypeException($contentType);
-        }
-        
-        try {
-            $soapMessage = $this->getSoapMessage();
-        } catch (\Exception $e) {
-            throw new Exception\MissingSoapMessageException(sprintf("Error loading SOAP message: [%s] %s", get_class($e), $e->getMessage()));
-        }
-    }
 }
